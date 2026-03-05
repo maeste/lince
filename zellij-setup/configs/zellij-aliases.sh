758075f270cbd66558355b4b753197ab0565feba
Alessio Soldano <services@soldano.it>
2026-03-05 15:00:59 +0100

    Translate to English

--- zellij-setup/configs/zellij-aliases.sh
+++ zellij-setup/configs/zellij-aliases.sh
@@ -1,5 +1,5 @@
 # Zellij aliases
 alias z='zellij'
 alias z3='zellij -n three-pane'
-alias zn='zellij attach -c'  # Attach o create new session
+alias zn='zellij attach -c'  # Attach or create new session
 alias zz3='zellij -n three-pane-zai'
